aefc6d76c36690b9e193c1e7b42da5f64a91eb58
user <user@DESKTOP-LUJ7RT6>
2018-09-18 14:28:48 +0700

    telkom

---
+++ src/KonversiSuhu.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class KonversiSuhu {
+    public static void main(String[] args){
+        double celcius,reamur,farenheit,kelvin;
+        String hasil;
+        celcius = 78;
+        Scanner hitung = new Scanner (System.in);
+        System.out.println("Reamur = "+ (4*celcius / 5));
+        System.out.println("Farenheit = "+(9*celcius / 5+32));
+        System.out.println("Kelvin = "+(celcius+273));
+       
+        
+        
+    }
+}
