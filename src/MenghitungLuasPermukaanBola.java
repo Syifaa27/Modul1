1e5d8ca809d096e3a25f22d9442f8156d9d7e32b
user <user@DESKTOP-LUJ7RT6>
2018-09-18 14:24:19 +0700

    telkom

--- src/MenghitungLuasPermukaanBola.java
+++ src/MenghitungLuasPermukaanBola.java
@@ -5,12 +5,13 @@ public class MenghitungLuasPermukaanBola {
         double phi = 3.14;
       Scanner hitung =new Scanner(System.in);
       System.out.println("SYIFA' AULIYA ASH-SHOLIHAH");
-        System.out.print("Masukan jari-jarinya:" );
+        System.out.print("Masukan jari-jarinya:");
         r= hitung.nextInt();
-        System.out.print("===========================");
-        System.out.print("Mengitung Luas Permukaan Bola");
-        System.out.print("============================");
+        System.out.println("===========================");
+        System.out.println("Mengitung Luas Permukaan Bola");
+        System.out.println("============================");
         System.out.print("Luas Permukaan Bola= "+(4*phi*r*r));
+        
         
     }
 
