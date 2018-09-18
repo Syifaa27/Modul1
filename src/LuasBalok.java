ddf761cf7675517a4a7321eaf0fd369b8f72cc74
user <user@DESKTOP-LUJ7RT6>
2018-09-18 14:16:01 +0700

    telkom

---
+++ src/LuasBalok.java
@@ -0,0 +1,15 @@
+
+public class LuasBalok {
+    public static void main(String[]args) {
+        int p=5;
+        int l=10;
+        int t=5;
+        System.out.print("Nilai Panjang Balok:"+p);
+        System.out.print("Nilai Lebar Balok:"+l);
+        System.out.print("Nilai Tingi Balok:"+t);
+        System.out.print("===========================");
+        System.out.print("Mengitung Luas Permukaan Balok");
+        System.out.print("============================");
+        System.out.print("Luas Permukaan Balok= "+((2*p*l)+(2*p*t)+(2*l*t)));
+    }
+}
