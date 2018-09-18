c8e0028d907c4e8fd4e33eaee218232fdd10f815
user <user@DESKTOP-LUJ7RT6>
2018-09-18 14:14:55 +0700

    telkom

---
+++ src/Variabel.java
@@ -0,0 +1,11 @@
+
+public class Variabel {
+    public static void main (String[] args) {
+        int nilai_pbo, nilai_web;
+        nilai_pbo = 90;
+        nilai_web =80;
+        System.out.println("Variabel yang terdapat dalam program :");
+        System.out.println("nilsi_pbo = " + nilai_pbo);
+        System.out.println("nilsi_web = " + nilai_web);
+    }
+}
